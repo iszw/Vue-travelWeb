dce0bb65eaf63db0905a7c5698a4d37acd93bf9c
1996szw <1445427773@qq.com>
2018-08-24 01:26:26 +0800

    add header

--- src/main.js
+++ src/main.js
@@ -4,8 +4,9 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastClick from 'fastclick' // 解决移动端点击300ms延迟的缺陷
-import './assets/styles/reset.css' // reset.css
-import './assets/styles/border.css' // 1像素边框的解决方案
+import 'styles/reset.css' // reset.css
+import 'styles/border.css' // 1像素边框的解决方案
+import 'styles/iconfont.css'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
